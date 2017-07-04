976815837bbdaae8f4cb952dadce2d56f4f258e8
tomhoff <tmsz.hof.gmail.com>
2017-07-04 09:24:07 +0200

    Zadanie1

--- zadanie1/index.php
+++ zadanie1/index.php
@@ -1,9 +1,34 @@
-<?php
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Zadanie 1</title>
+</head>
+<body>
 
+    <form action="index.php" method="post">
+        <label><h2>Proszę o podanie imienia:</h2>
+            <input type="text" name="imie" placeholder="-->Tu wpisz swoje imię...">
+        </label>
+    </form>
 
-echo '<form>
-        <label>Podaj imię:
-            <input type="text" name='name' placeholder='Imię'>
+    <?php
 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-        </form>';
+        if (isset($_POST['imie'])){
+
+            $imie=$_POST['imie'];
+
+            echo "<h1>Cześć, $imie!</h1>";
+        }
+
+    }
+
+    ?>
+
+</body>
+</html>
